0f39146e6f17e59e040cad41ef76a57ed4eec43e
icmoore <ic3moore@gmail.com>
2024-07-09 13:56:07 -0700

    more endpoints

--- python/prod/modules/stats.py
+++ python/prod/modules/stats.py
@@ -1,7 +1,19 @@
-from blockscout.enums.actions_enum import ActionsEnum as actions
-from blockscout.enums.fields_enum import FieldsEnum as fields
-from blockscout.enums.modules_enum import ModulesEnum as modules
+from blockscout.enums.actions_enum import ActionsEnum as Actions
+from blockscout.enums.rpc_fields_enum import RPCFieldsEnum as RPCFields
+from blockscout.enums.rest_fields_enum import RestFieldsEnum as RestFields
+from blockscout.enums.modules_enum import ModulesEnum as Modules
+from blockscout.enums.tags_enum import TagsEnum as Tags
+
 
 
 class Stats:
-    pass
+    
+    @staticmethod
+    def get_total_token_supply() -> str:
+        url = (
+            f"{RPCFields.MODULE}"
+            f"{Modules.STATS}"
+            f"{RPCFields.ACTION}"
+            f"{Actions.TOKEN_SUPPLY}"
+        )
+        return url
