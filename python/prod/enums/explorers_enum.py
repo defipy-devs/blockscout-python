b9c8423b11bfad53ad08f729ac4f587106f22423
icmoore <ic3moore@gmail.com>
2024-07-24 16:18:42 -0700

    coin transfers

--- python/prod/enums/explorers_enum.py
+++ python/prod/enums/explorers_enum.py
@@ -8,6 +8,10 @@ class ExplorersEnum:
     ROLLUX: str = "explorer.rollux.com/"
     MAIN: str = "eth.blockscout.com/"
     GOERLI: str = "eth-goerli.blockscout.com/"
+    
+    ROLLUX_COIN_DAT = {'coin_symbol': 'SYS', 'coin_name': 'Syscoin', 'coin_decimal': 18}
+    MAIN_COIN_DAT = {'coin_symbol': 'ETH', 'coin_name': 'Ethereum', 'coin_decimal': 18}
+    GOERLI_COIN_DAT = {'coin_symbol': 'ETH', 'coin_name': 'Ethereum', 'coin_decimal': 18}
 
     def get_explorer(self, net = DEFAULT_NET) -> str:
              
@@ -20,3 +24,16 @@ class ExplorersEnum:
                 select_explorer = self.GOERLI 
 
         return select_explorer 
+
+
+    def get_coin_dat(self, net = DEFAULT_NET) -> str:
+             
+        match net:
+            case NetsEnum.ROLLUX:
+                select_coin_dat = self.ROLLUX_COIN_DAT
+            case NetsEnum.MAIN:
+                select_coin_dat = self.MAIN_COIN_DAT  
+            case NetsEnum.GOERLI:
+                select_coin_dat = self.GOERLI_COIN_DAT 
+
+        return select_coin_dat 
